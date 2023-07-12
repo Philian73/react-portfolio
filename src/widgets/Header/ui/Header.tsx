9d8baee8689a3fd516523e335f6bc9a0fda6bdad
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-13 00:08:07 +0300

    semantic

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,13 +2,13 @@ import cls from './Header.module.scss'
 
 export const Header = () => {
   return (
-    <div className={cls.header}>
-      <div className={cls.nav}>
+    <header className={cls.header}>
+      <nav className={cls.nav}>
         <a href="#">Home</a>
         <a href="#">Skills</a>
         <a href="#">Projects</a>
         <a href="#">Contacts</a>
-      </div>
-    </div>
+      </nav>
+    </header>
   )
 }
