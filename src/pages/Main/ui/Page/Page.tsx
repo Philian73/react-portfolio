44fba019e51d67960c6573bae94b4e898d5a9f1c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-13 23:56:15 +0300

    Import Skill component

--- src/pages/Main/ui/Page/Page.tsx
+++ src/pages/Main/ui/Page/Page.tsx
@@ -1,3 +1,4 @@
+import { Skill } from '../../../../entities/Skill/ui/Skill'
 import { Header } from '../../../../widgets/Header'
 
 import cls from './Page.module.scss'
@@ -6,16 +7,37 @@ export const MainPage = () => {
   return (
     <>
       <Header />
-      <div className={cls.mainBlock}>
-        <div className={cls.container}>
-          <div className={cls.body}>
-            <span>Hi There</span>
-            <h1>I am Nikolay Sulteev</h1>
-            <p>Frontend Developer</p>
+      <main>
+        <section className={cls.mainBlock}>
+          <div className={cls.mainBlock_container}>
+            <div className={cls.body}>
+              <span>Hi There</span>
+              <h1>I am Nikolay Sulteev</h1>
+              <p>Frontend Developer</p>
+            </div>
+            <div className={cls.images}></div>
           </div>
-          <div className={cls.images}></div>
-        </div>
-      </div>
+        </section>
+        <section className={cls.skillsBlock}>
+          <div className={cls.skillsBlock_container}>
+            <h2 className={cls.title}>My Skills</h2>
+            <div className={cls.skills}>
+              <Skill
+                title="HTML"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing."
+              />
+              <Skill
+                title="CSS"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing."
+              />
+              <Skill
+                title="JS"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing."
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
