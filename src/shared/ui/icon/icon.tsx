b4b1d85e2051d67f244f00d93ecbb238cf476bc7
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-14 21:51:45 -1200

    created Icon.tsx component

---
+++ src/shared/ui/icon/icon.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, memo, Ref, SVGProps } from 'react'
+
+import { SvgIdsType } from './'
+
+import icons from '@/assets/icons/sprite.svg'
+
+const SvgComponent = (
+  { id, ...rest }: SVGProps<SVGSVGElement> & { id: SvgIdsType },
+  ref: Ref<SVGSVGElement>
+) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    ref={ref}
+    {...rest}
+  >
+    <use xlinkHref={icons + `#${id}`}></use>
+  </svg>
+)
+
+export const Icon = memo(forwardRef(SvgComponent))
